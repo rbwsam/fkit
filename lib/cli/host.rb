7d21f1b198b759f9a3fd98c6884180bdbeefe45a
Sam Leavens <3139851+rbwsam@users.noreply.github.com>
2023-07-09 18:05:45 +0200

    WIP

--- lib/cli/host.rb
+++ lib/cli/host.rb
@@ -1,11 +1,14 @@
 require_relative 'sub_command_base'
+require_relative 'config_file'
 
 module CLI
   class Host < SubCommandBase
-    desc "add", "Add a new host to the list of known hosts"
+    desc "add URI", "Add a new host to the list of known hosts"
 
-    def add
-      puts "adding a host..."
+    def add(uri)
+      c = ConfigFile.load_file 'fkit.yaml'
+      c.data[:hosts].append(uri)
+      c.save('fkit.yaml')
     end
   end
 end
